2f23a90b23401b91f5dbb37dea780721b9c2ac5d
AndersonTM30 <andersondev30@gmail.com>
2024-02-06 13:03:59 -0300

    refact: adicionando teste de integração para o método de criar usuário

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersModule } from './users.module';
+import { PrismaService } from '../prisma_client/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [UsersModule],
+      providers: [UsersService, PrismaService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +17,13 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a new user', async () => {
+    const newUser = await service.createUser({
+      username: 'Anderson',
+      password: '1234',
+    });
+    expect(newUser.username).toEqual('Anderson');
   });
 });
