b2aca2fa6941eae3e4cc0850f75ca38b4fbbc638
AndersonTM30 <andersondev30@gmail.com>
2024-02-09 08:49:47 -0300

    refact: adicionando o dto de users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, HttpCode } from '@nestjs/common';
 import { Users } from '@prisma/client';
 import { UsersService } from './users.service';
+import { CreateUsersDto } from './dto/create.users.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async signupUser(
-    @Body() userData: { username: string; password: string },
-  ): Promise<Users> {
+  @HttpCode(201)
+  async signupUser(@Body() userData: CreateUsersDto): Promise<Users> {
     return this.usersService.createUser(userData);
   }
 }
