933fb909267b2b73ba814bceffbb0294ac066f1d
AndersonTM30 <andersondev30@gmail.com>
2024-02-09 08:50:18 -0300

    test: adicionando teste end to end da rota de users

--- src/users/test/e2e/users.controller.spec.ts
+++ src/users/test/e2e/users.controller.spec.ts
@@ -1,20 +1,53 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from '../../users.controller';
+import { Test } from '@nestjs/testing';
 import { UsersModule } from '../../users.module';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+// import { UsersService } from '../../../users/users.service';
+import { CreateUsersDto } from '../../../users/dto/create.users.dto';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe('UsersController (e2e)', () => {
+  let app: INestApplication;
+  // let usersService: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
       imports: [UsersModule],
-      controllers: [UsersController],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/POST users', async () => {
+    const createUsersDto: CreateUsersDto = {
+      username: 'Anderson',
+      password: '1234',
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send(createUsersDto);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.username).toEqual(createUsersDto.username);
+  });
+
+  it('/users POST - password should not be empty', async () => {
+    const invalidUserData = {
+      username: 'Anderson',
+      password: '',
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send(invalidUserData);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toContain('password should not be empty');
   });
 });
