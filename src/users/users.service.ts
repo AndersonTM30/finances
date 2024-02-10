fed0fb6e9e276707e1c7b4519e6bf882baba77a0
Anderson Ferreira <anderson@pop-os.localdomain>
2024-02-10 01:02:22 -0300

    refactor: removendo a senha do retorno da criação de usuário

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma_client/prisma.service';
-import { Users } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { CreateUsersDto } from './dto/create.users.dto';
+import { UserOutputDto } from './dto/user.output.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(data: CreateUsersDto): Promise<Users> {
+  async createUser(data: CreateUsersDto): Promise<UserOutputDto> {
     const saltOrRounds = 10;
     const hashedPassword = await bcrypt.hash(data.password, saltOrRounds);
     data.password = hashedPassword;
 
-    return this.prisma.users.create({
+    const user = await this.prisma.users.create({
       data,
     });
+    return {
+      id: user.id,
+      username: user.username,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 
   async findByUsername(username: string) {
