4bb3da69d11787acc2ce202e37ffe33339b5553e
AndersonTM30 <andersondev30@gmail.com>
2024-02-16 15:44:28 -0300

    refactor(login.dto.ts): adicionando as propriedades no swagger

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,11 +3,24 @@ import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty()
-  @ApiProperty({ type: String, format: 'string', example: 'username' })
+  @ApiProperty({
+    type: String,
+    format: 'string',
+    example: 'username',
+    description: 'The username',
+    maxLength: 50,
+  })
   username: string;
 
   @IsNotEmpty()
   @MinLength(8)
-  @ApiProperty({ type: String, format: 'password', example: 'password' })
+  @ApiProperty({
+    type: String,
+    format: 'password',
+    example: 'password',
+    description: 'The password',
+    minLength: 8,
+    maxLength: 60,
+  })
   password: string;
 }
