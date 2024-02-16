af9f821720342bf552066f75ad68ad1987fc8848
AndersonTM30 <andersondev30@gmail.com>
2024-02-16 15:45:33 -0300

    feat(auth.controller.ts): adicionando validação na rota de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ApiBody, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthEntity } from './entity/auth.entity';
@@ -12,6 +20,15 @@ export class AuthController {
   @Post('login')
   @ApiOkResponse({ type: AuthEntity })
   @ApiBody({ type: LoginDto })
+  @HttpCode(201)
+  @UsePipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => new BadRequestException(errors),
+    }),
+  )
   async login(@Body() { username, password }: LoginDto): Promise<AuthEntity> {
     return this.authService.login(username, password);
   }
