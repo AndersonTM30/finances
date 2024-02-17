50ed80507723703ba36014fa84a0bd7972183614
Anderson Ferreira <anderson@pop-os.localdomain>
2024-02-17 17:41:53 -0300

    refactor(auth.controller.spec.ts): refatorando a rota de login removendo os mocks e adicionando os cookies

--- src/auth/test/e2e/auth.controller.spec.ts
+++ src/auth/test/e2e/auth.controller.spec.ts
@@ -1,31 +1,19 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AuthService } from '../../../auth/auth.service';
-import { AuthController } from '../../../auth/auth.controller';
 import { LoginDto } from '../../../auth/dto/login.dto';
 import * as request from 'supertest';
+import { AppModule } from '../../../app.module';
 
 describe('AuthController', () => {
   let app: INestApplication;
-  let authService: AuthService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [
-        {
-          provide: AuthService,
-          useValue: {
-            login: jest.fn().mockResolvedValue({ accessToken: 'testToken' }),
-          },
-        },
-      ],
+      imports: [AppModule],
     }).compile();
 
     app = module.createNestApplication();
     await app.init();
-
-    authService = module.get<AuthService>(AuthService);
   });
 
   afterAll(async () => {
@@ -38,20 +26,19 @@ describe('AuthController', () => {
 
   it('should return an access token if login is successful', async () => {
     const loginDto: LoginDto = {
-      username: 'Anderson',
-      password: 'testPassword',
+      username: 'belmont',
+      password: '12345678',
     };
 
     const response = await request(app.getHttpServer())
       .post('/auth/login')
       .send(loginDto);
 
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty('accessToken');
-    expect(response.body.accessToken).toBe('testToken');
-    expect(authService.login).toHaveBeenCalledWith(
-      loginDto.username,
-      loginDto.password,
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty(
+      'message',
+      'Login efetuado com sucesso!',
     );
+    expect(response.body.accessToken).toBeDefined();
   });
 });
