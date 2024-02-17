13c10e9f3add80026518a19d2506b4ff6098c7d2
Anderson Ferreira <anderson@pop-os.localdomain>
2024-02-17 01:13:10 -0300

    feat: adicionando a funcionalidade de refresh token nos cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,23 +4,37 @@ import {
   Controller,
   HttpCode,
   Post,
+  Req,
+  Res,
+  UnauthorizedException,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiBody, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthEntity } from './entity/auth.entity';
 import { LoginDto } from './dto/login.dto';
+import { Request, Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('login')
   @ApiOkResponse({ type: AuthEntity })
   @ApiBody({ type: LoginDto })
-  @HttpCode(201)
+  @HttpCode(200)
   @UsePipes(
     new ValidationPipe({
       whitelist: true,
@@ -29,7 +43,51 @@ export class AuthController {
       exceptionFactory: (errors) => new BadRequestException(errors),
     }),
   )
-  async login(@Body() { username, password }: LoginDto): Promise<AuthEntity> {
-    return this.authService.login(username, password);
+  async login(
+    @Body() { username, password }: LoginDto,
+    @Res() res: Response,
+  ): Promise<AuthEntity> {
+    const authEntity = await this.authService.login(username, password, res);
+    return authEntity;
+  }
+
+  @Post('refresh')
+  @ApiOperation({ summary: 'Refresh the access token' })
+  @ApiResponse({
+    status: 200,
+    description: 'The access token has been refreshed.',
+    type: AuthEntity,
+  })
+  async refreshToken(@Req() req: Request): Promise<AuthEntity> {
+    const refreshToken = req.cookies['refresh_token'];
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token not found');
+    }
+
+    try {
+      const payload = this.jwtService.verify(refreshToken);
+      const user = await this.authService.validateUser(payload.userId);
+
+      const accessToken = this.jwtService.sign({ userId: user.id });
+      const newRefreshToken = this.jwtService.sign(
+        { userId: user.id },
+        { expiresIn: '7d' },
+      );
+
+      req.res.cookie('refresh_token', newRefreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 604800000,
+      });
+
+      return {
+        accessToken,
+        refreshToken: newRefreshToken,
+      };
+    } catch (error) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
   }
 }
