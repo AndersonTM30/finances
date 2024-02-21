24aa7c763a4f5d1f05896c85a29d776b5d8f0f28
Anderson Ferreira <anderson@pop-os.localdomain>
2024-02-20 23:16:08 -0300

    refactor(auth.entity): adidionando documentação de api

--- src/auth/entity/auth.entity.ts
+++ src/auth/entity/auth.entity.ts
@@ -1,9 +1,21 @@
 ﻿import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthEntity {
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    format: 'string',
+    description: 'Authorization token',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTcwODQ4MTQwM30.dQC7DscHRSr3S4cTea-PhVFWdlwvWeYKLfyQz7tZBH4',
+  })
   accessToken: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    format: 'string',
+    description: 'Refresh token',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTcwODQ4MTQwMywiZXhwIjoxNzA5MDg2MjAzfQ.sKAY3zRPuo_AuezZsymnZllpJXFmnFgb1DTBUePh46s',
+  })
   refreshToken: string;
 }
