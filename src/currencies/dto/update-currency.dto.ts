0d9c4803ff2b084fb2f40b6c9698055c6d294fa2
Anderson Ferreira <anderson@pop-os.localdomain>
2024-02-23 11:20:42 -0300

    refactor(update-currency.dto): adicionando validação no campo name

--- src/currencies/dto/update-currency.dto.ts
+++ src/currencies/dto/update-currency.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCurrencyDto } from './create-currency.dto';
+import { IsNotEmpty } from 'class-validator';
 
 export class UpdateCurrencyDto extends PartialType(CreateCurrencyDto) {
+  @IsNotEmpty()
   name: string;
 }
