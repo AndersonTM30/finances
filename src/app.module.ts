d72892b308f96f98998f93b687fa0155d03ced05
Anderson Ferreira <anderson@pop-os.localdomain>
2024-02-23 17:08:47 -0300

    feat(app.module): adicionando o módulo de categoria de renda

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { CurrenciesModule } from './currencies/currencies.module';
+import { CategoriesIncomesModule } from './categories.incomes/categories.incomes.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CurrenciesModule],
+  imports: [AuthModule, UsersModule, CurrenciesModule, CategoriesIncomesModule],
   controllers: [UsersController],
 })
 export class AppModule {}
