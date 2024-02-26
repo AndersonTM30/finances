48600172960ec5f42374ee2521d7b9749bd4a073
AndersonTM30 <andersondev30@gmail.com>
2024-02-26 12:26:19 -0300

    feat(categories.expenses.service): adicionando o crud de categorias de despesas

--- src/categories.expenses/categories.expenses.service.ts
+++ src/categories.expenses/categories.expenses.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCategoriesExpenseDto } from './dto/create-categories.expense.dto';
 import { UpdateCategoriesExpenseDto } from './dto/update-categories.expense.dto';
 import { PrismaService } from '../prisma_client/prisma.service';
@@ -6,7 +10,7 @@ import { PrismaService } from '../prisma_client/prisma.service';
 @Injectable()
 export class CategoriesExpensesService {
   constructor(private prisma: PrismaService) {}
-  create(data: CreateCategoriesExpenseDto) {
+  async create(data: CreateCategoriesExpenseDto) {
     if (!data.name) {
       throw new BadRequestException('Name is not empty');
     }
@@ -14,19 +18,73 @@ export class CategoriesExpensesService {
     return this.prisma.categories_Expenses.create({ data });
   }
 
-  findAll() {
-    return `This action returns all categoriesExpenses`;
+  async findAll() {
+    return this.prisma.categories_Expenses.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+      orderBy: {
+        name: 'asc',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} categoriesExpense`;
+  async findOne(id: number) {
+    const categoriesExpenses = await this.prisma.categories_Expenses.findUnique(
+      {
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    );
+
+    if (!categoriesExpenses) {
+      throw new NotFoundException();
+    }
+
+    return categoriesExpenses;
   }
 
-  update(id: number, updateCategoriesExpenseDto: UpdateCategoriesExpenseDto) {
-    return `This action updates a #${id} categoriesExpense`;
+  async update(
+    id: number,
+    updateCategoriesExpenseDto: UpdateCategoriesExpenseDto,
+  ) {
+    const categoryExpense = await this.prisma.categories_Expenses.findUnique({
+      where: { id },
+    });
+
+    if (!categoryExpense) {
+      throw new NotFoundException('Category of Expense not found');
+    }
+
+    if (!updateCategoriesExpenseDto.name) {
+      throw new BadRequestException('Name is not empty!');
+    }
+
+    return this.prisma.categories_Expenses.update({
+      where: {
+        id: id,
+      },
+      data: { name: updateCategoriesExpenseDto.name, updatedAt: new Date() },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} categoriesExpense`;
+  async remove(id: number) {
+    const categoryExpense = await this.prisma.categories_Expenses.findUnique({
+      where: { id },
+    });
+
+    if (!categoryExpense) {
+      throw new NotFoundException('Category of expense not found');
+    }
+
+    return this.prisma.categories_Expenses.delete({
+      where: { id },
+    });
   }
 }
