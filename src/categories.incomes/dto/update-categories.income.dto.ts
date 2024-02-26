4184fc9a6f5c269d24992ae85b307b9a8c2a776f
AndersonTM30 <andersondev30@gmail.com>
2024-02-26 07:28:11 -0300

    refactor(update-categories.income.dto.ts): ajustando a identação da classe

--- src/categories.incomes/dto/update-categories.income.dto.ts
+++ src/categories.incomes/dto/update-categories.income.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCategoriesIncomeDto } from './create-categories.income.dto';
 
-export class UpdateCategoriesIncomeDto extends PartialType(CreateCategoriesIncomeDto) {}
+export class UpdateCategoriesIncomeDto extends PartialType(
+  CreateCategoriesIncomeDto,
+) {}
