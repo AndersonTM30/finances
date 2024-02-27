7e361dac83c2840932abb2601f3f50d804e0a934
AndersonTM30 <andersondev30@gmail.com>
2024-02-27 08:41:00 -0300

    refactor(incomes.service): adicionando validação no método de cadastrar renda

--- src/incomes/incomes.service.ts
+++ src/incomes/incomes.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateIncomeDto } from './dto/create-income.dto';
 import { UpdateIncomeDto } from './dto/update-income.dto';
+import { PrismaService } from '../prisma_client/prisma.service';
+import { NotEmptyField } from './validations/not.empty.field';
 
 @Injectable()
 export class IncomesService {
-  create(createIncomeDto: CreateIncomeDto) {
-    return 'This action adds a new income';
+  constructor(
+    private prisma: PrismaService,
+    private notEmptyField: NotEmptyField,
+  ) {}
+
+  async create(data: CreateIncomeDto) {
+    this.notEmptyField.validationEmptyFiledDescription(data.description);
+    this.notEmptyField.validationEmptyFiledCategoryId(data.categoryId);
+    this.notEmptyField.validationEmptyFiledUserId(data.userId);
+    this.notEmptyField.validationEmptyFiledCurrencyId(data.currencyId);
+    this.notEmptyField.validationEmptyFiledDate(data.date);
+    this.notEmptyField.validationEmptyFiledValue(data.value);
+    return this.prisma.incomes.create({ data });
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all incomes`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} income`;
   }
 
-  update(id: number, updateIncomeDto: UpdateIncomeDto) {
+  async update(id: number, updateIncomeDto: UpdateIncomeDto) {
     return `This action updates a #${id} income`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} income`;
   }
 }
