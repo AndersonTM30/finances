ac82784b5a97e65a7e01fe28818a34a728ab8e8e
AndersonTM30 <andersondev30@gmail.com>
2024-02-27 08:34:49 -0300

    feat(incomes.module): adicionando o provedor de validação de campos no módulo de renda

--- src/incomes/incomes.module.ts
+++ src/incomes/incomes.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IncomesService } from './incomes.service';
 import { IncomesController } from './incomes.controller';
+import { PrismaService } from '../prisma_client/prisma.service';
+import { NotEmptyField } from './validations/not.empty.field';
 
 @Module({
   controllers: [IncomesController],
-  providers: [IncomesService],
+  providers: [IncomesService, PrismaService, NotEmptyField],
 })
 export class IncomesModule {}
