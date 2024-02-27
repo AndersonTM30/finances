24a1debfe265ebb48964f99f324b92a73d68212f
AndersonTM30 <andersondev30@gmail.com>
2024-02-27 08:33:40 -0300

    feat(not.empty.field): criando validação para os campos do formulário de cadastro de renda

---
+++ src/incomes/validations/not.empty.field.ts
@@ -0,0 +1,45 @@
+﻿import { BadRequestException } from '@nestjs/common';
+
+export class NotEmptyField {
+  validationEmptyFiledDescription(field: string) {
+    if (!field) {
+      throw new BadRequestException(`Description is not empty`);
+    }
+    return field;
+  }
+
+  validationEmptyFiledCategoryId(field: number) {
+    if (!field) {
+      throw new BadRequestException(`CategoryId is not empty`);
+    }
+    return field;
+  }
+
+  validationEmptyFiledUserId(field: number) {
+    if (!field) {
+      throw new BadRequestException(`UserId is not empty`);
+    }
+    return field;
+  }
+
+  validationEmptyFiledCurrencyId(field: number) {
+    if (!field) {
+      throw new BadRequestException(`CurrencyId is not empty`);
+    }
+    return field;
+  }
+
+  validationEmptyFiledDate(field: string) {
+    if (!field) {
+      throw new BadRequestException(`Date is not empty`);
+    }
+    return field;
+  }
+
+  validationEmptyFiledValue(field: number) {
+    if (!field) {
+      throw new BadRequestException(`Value is not empty`);
+    }
+    return field;
+  }
+}
