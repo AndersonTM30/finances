2322a7e60aa542b082a4527afc393eb0a00918fd
AndersonTM30 <andersondev30@gmail.com>
2024-02-28 13:30:03 -0300

    test(incomes.controller.e2e.spec): adicionei os testes de listar todas as rendas e listar renda por id

--- src/incomes/test/incomes.controller.e2e.spec.ts
+++ src/incomes/test/incomes.controller.e2e.spec.ts
@@ -1,20 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { IncomesController } from '../incomes.controller';
-import { IncomesService } from '../incomes.service';
+import { INestApplication } from '@nestjs/common';
+import { CreateIncomeDto } from '../dto/create-income.dto';
+import { UsersModule } from '../../users/users.module';
+import { IncomesModule } from '../incomes.module';
+import { AuthModule } from '../../auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import * as request from 'supertest';
 
-describe('IncomesController', () => {
-  let controller: IncomesController;
+describe('IncomesController (e2e)', () => {
+  let app: INestApplication;
+  let token: string;
+  let createIncomeDto: CreateIncomeDto;
+  const invalidToken = 'slçakjaçljkdahlçkjadçlkhjdfa';
+  const route = '/incomes';
+  let createcategoryIncomeDto = { name: 'Salário' };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [IncomesController],
-      providers: [IncomesService],
+      imports: [
+        UsersModule,
+        IncomesModule,
+        AuthModule,
+        PassportModule.register({
+          defaultStrategy: 'jwt',
+        }),
+        JwtModule.register({
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
     }).compile();
 
-    controller = module.get<IncomesController>(IncomesController);
+    app = module.createNestApplication();
+    await app.init();
+
+    const loginResponse = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ username: 'Anderson', password: '12341245' });
+
+    token = loginResponse.body.accessToken;
+
+    createIncomeDto = {
+      description: 'Pagamento de Boleto',
+      categoryId: 48,
+      userId: 84,
+      currencyId: 5,
+      date: '2024-02-27T10:08:00.777Z',
+      value: 260.0,
+    };
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/incomes GET - should return the list of all incomes', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`${route}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+  });
+
+  it('/incomes GET - should return the unauthorized message', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`${route}`)
+      .set('Authorization', `Bearer ${invalidToken}`);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.message).toEqual('Unauthorized');
+  });
+
+  it('/incomes/:id GET - should be return income by id', async () => {
+    // console.log('Create Income DTO ', createIncomeDto);
+    // const resonseCreateCategoryIncome = await request(app.getHttpServer())
+    //   .post('/categories/incomes')
+    //   .send(createcategoryIncomeDto)
+    //   .set('Authorization', `Bearer ${token}`);
+    // createIncomeDto.categoryId = resonseCreateCategoryIncome.body.id;
+    // console.log(createIncomeDto.categoryId);
+
+    const responseIncome = await request(app.getHttpServer())
+      .post(route)
+      .set('Authorization', `Bearer ${token}`)
+      .send(createIncomeDto);
+    console.log('Response: ', responseIncome.body);
+
+    const incomeId = responseIncome.body.id;
+
+    const response = await request(app.getHttpServer())
+      .get(`${route}/${incomeId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    await request(app.getHttpServer())
+      .delete(`${route}/${incomeId}`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
   });
 });
