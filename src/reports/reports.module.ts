b65be3611053ced9f7180d369ec5f568e5a92375
AndersonTM30 <andersondev30@gmail.com>
2024-03-05 16:21:31 -0300

    feat(reports.module): adicionando os providers de prisma service e validate fields

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { PrismaService } from '../prisma_client/prisma.service';
+import { ValidateFields } from './validations/validate.fields';
 
 @Module({
   controllers: [ReportsController],
-  providers: [ReportsService],
+  providers: [ReportsService, PrismaService, ValidateFields],
 })
 export class ReportsModule {}
