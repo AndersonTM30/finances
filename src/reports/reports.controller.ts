426b09dbe295839614ad6572db3ca615ee577699
AndersonTM30 <andersondev30@gmail.com>
2024-03-05 16:25:33 -0300

    feat(reports.controller): adicionando a rota que consulta o resumo por período

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+  @Get()
+  async findAll(
+    @Query('userId', ParseIntPipe) userId: number,
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ) {
+    return this.reportsService.resume(startDate, endDate, userId);
+  }
 }
