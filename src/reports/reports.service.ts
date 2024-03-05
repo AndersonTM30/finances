c187327a22a8cb3c0fff6c3aa237b5fda4ba3c8d
AndersonTM30 <andersondev30@gmail.com>
2024-03-05 16:22:31 -0300

    feta(reports.service):Adicionando a consulta de resumo por período

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma_client/prisma.service';
+import { ValidateFields } from './validations/validate.fields';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    private prisma: PrismaService,
+    private validateFields: ValidateFields,
+  ) {}
+
+  async resume(startDate: string, endDate: string, userId: number) {
+    const startDateFormated =
+      this.validateFields.convertBrazilianDateToJSDate(startDate);
+    const endDateFormated =
+      this.validateFields.convertBrazilianDateToJSDate(endDate);
+    const incomesSum = await this.prisma.incomes.aggregate({
+      where: {
+        userId: userId,
+        date: {
+          gte: startDateFormated,
+          lte: endDateFormated,
+        },
+      },
+      _sum: {
+        value: true,
+      },
+    });
+
+    const expensesSum = await this.prisma.expenses.aggregate({
+      where: {
+        userId: userId,
+        date: {
+          gte: startDateFormated,
+          lte: endDateFormated,
+        },
+      },
+      _sum: {
+        value: true,
+      },
+    });
+
+    const difference = incomesSum._sum.value - expensesSum._sum.value;
+
+    return {
+      incomes: incomesSum._sum.value,
+      expenses: expensesSum._sum.value,
+      total: difference,
+    };
+  }
+}
