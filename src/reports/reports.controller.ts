7c81980939c78bfc0fc3241151c0877749608e42
AndersonTM30 <andersondev30@gmail.com>
2024-03-10 01:49:30 -0300

    feat(reports.controller): adicionando as rotas de renda e despesa por categoria e a rota de transações por período

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,12 +4,52 @@ import { ReportsService } from './reports.service';
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
-  @Get()
+
+  @Get('balance')
   async findAll(
     @Query('startDate') startDate: string,
     @Query('endDate') endDate: string,
     @Query('userId', ParseIntPipe) userId: number,
   ) {
     return this.reportsService.resume(startDate, endDate, userId);
+  }
+
+  @Get('incomes/category')
+  async findByIncomesCategoryName(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+    @Query('categoryName') categoryName: string,
+    @Query('userId', ParseIntPipe) userId: number,
+  ) {
+    return this.reportsService.getIncomesByCategoryName(
+      startDate,
+      endDate,
+      categoryName,
+      userId,
+    );
+  }
+
+  @Get('expenses/category')
+  async findByExpensesCategoryName(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+    @Query('categoryName') categoryName: string,
+    @Query('userId', ParseIntPipe) userId: number,
+  ) {
+    return this.reportsService.getExpensesByCategoryName(
+      startDate,
+      endDate,
+      categoryName,
+      userId,
+    );
+  }
+
+  @Get('transactions')
+  async findTransactionsByPeriod(
+    @Query('userId', ParseIntPipe) userId: number,
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ) {
+    return this.reportsService.getCombinedReport(userId, startDate, endDate);
   }
 }
