3a4bb7a33e8cb29bb8efedb9a9823d3e65d87dcb
AndersonTM30 <andersondev30@gmail.com>
2024-03-10 01:47:04 -0300

    refactor(validate.fields): formatando o pardrão da data para consulta no banco de dados

--- src/reports/validations/validate.fields.ts
+++ src/reports/validations/validate.fields.ts
@@ -1,6 +1,16 @@
 export class ValidateFields {
   convertBrazilianDateToJSDate(dateString: string) {
     const [day, month, year] = dateString.split('/');
-    return new Date(`${year}-${month}-${day}`);
+    const date = new Date(
+      Date.UTC(parseInt(year), parseInt(month) - 1, parseInt(day), 0, 0, 0),
+    );
+    date.setUTCDate(date.getUTCDate() + 1);
+    date.setUTCHours(0, 0, 0, 0);
+    return date;
+  }
+
+  convertJSDateToBrazilianDate(dateString: string) {
+    const [day, month, year] = dateString.split('-');
+    return new Date(`${day}/${month}/${year}`);
   }
 }
